386d5ec69dc0bed5b4cc2232193f0b4c9404aaf4
Hanna922 <hanna.urssu@gmail.com>
2023-02-13 22:10:16 +0900

    apply react-hook-form

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,25 +1,125 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function TodoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setToDoError("");
     setToDo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
         <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  checkPassword: string;
+  extraError?: string;
+}
+
+function TodoList() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    console.log(data);
+    if (data.password !== data.checkPassword) {
+      setError("checkPassword", {
+        message: "password are not the same",
+      });
+    }
+    // setError("extraError", { message: "server offline." });
+  };
+  // => react-hook-form이 모든 validation을 마쳤을 때만 호출될 것
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "wirte here",
+            validate: {
+              noHanna: (value) =>
+                value.includes("hanna") ? "no hannas allowed" : true,
+              noNayoung: (value) =>
+                value.includes("nayoung") ? "no nayoung allowed" : true,
+            },
+          })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", { required: "write here", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("checkPassword", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="check password"
+        />
+        <span>{errors?.checkPassword?.message}</span>
+        <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
 }
 
-export default ToDoList;
+export default TodoList;
